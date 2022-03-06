f03b987c06be2f03673b5270474cadee31ec3a48
Jose Federico <federicj@gmatc.matc.edu>
2022-03-05 23:59:05 -0600

    Final push

---
+++ public/main.js
@@ -0,0 +1,35 @@
+const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button')
+
+update.addEventListener('click', _ => {
+    fetch('/quotes', {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: 'Vaas',
+            quote: 'Insanity is doing the exact... same fucking thing... over and over again expecting... shit to change... That. Is. Crazy.'
+        })
+    })
+})
+
+
+deleteButton.addEventListener('click', _ => {
+    fetch('/quotes', {
+        method: 'delete',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: 'Vaas'
+        })
+    })
+        .then(res => {
+            if (res.ok) return res.json()
+        })
+        .then(response => {
+            if (response === 'No quote to delete') {
+                messageDiv.textContent = 'No quote to delete'
+            } else {
+                window.location.reload(true)
+            }
+        })
+        .catch(console.error)
+})
