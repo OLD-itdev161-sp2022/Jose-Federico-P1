4fe3ac115d89a9ecada9d216b494faec78da1b06
Jose Federico <federicj@gmatc.matc.edu>
2022-03-05 21:24:01 -0600

    initial setup files

---
+++ server.js
@@ -0,0 +1,27 @@
+// server.js
+console.log('May Node be with you')
+
+const express = require('express');
+const bodyParser= require('body-parser')
+const app = express();
+
+// Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(3000, function() {
+    console.log('listening on 3000')
+  });
+
+/*app.get('/', (req, res) => {
+    res.send('Hello World')
+})*/
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})
